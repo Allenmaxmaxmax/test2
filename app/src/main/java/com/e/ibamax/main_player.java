50f95c28965658414579e339bbedb1429845dee6
Allen <zxc093454@gmail.com>
2020-03-25 15:10:43 +0800

    第一次

--- app/src/main/java/com/e/ibamax/main_player.java
+++ app/src/main/java/com/e/ibamax/main_player.java
@@ -6,22 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.AdapterView;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class main_player extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.player);
 
         Spinner spinner =  findViewById(R.id.spinner);
         final String[] lunch = {"輔仁大學", "台灣大學", "師範大學", "屏東大學", "好棒棒球隊"};
-        ArrayAdapter<String> lunchList = new ArrayAdapter<>(MainActivity.this,
+        ArrayAdapter<String> lunchList = new ArrayAdapter<>(main_player.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 lunch);
         spinner.setAdapter(lunchList);
@@ -32,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, page2.class);
+                intent.setClass(main_player.this, page2.class);
                 startActivity(intent);
             }
         });
